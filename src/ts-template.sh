8855da5f02c6dee8f526c3da787f140c17753043
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2010-10-21 02:07:41 -0300

    ts rm for removing ~/files

--- src/ts-template.sh
+++ src/ts-template.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 dir_autoset=__ROOT__ # substituido pelo omake
+files_dir="$HOME/files"
 
 cd "$dir_autoset"
 
 c="$1"
 shift
 
-case $c in
+case "$c" in
     add) ./bin/ts-add "$@";;
+    rm) rm -r "$files_dir";;
     createdb) ./tools/createdb-simpler;;
     interface) ./tools/interface "$@";;
     *) cat "$0";;
