c3296a171fa42078c17b4dd4cbf84f8d918fe224
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2010-10-22 02:53:03 -0300

    minor (nice) improvements on web

--- web/web.rb
+++ web/web.rb
@@ -15,7 +15,7 @@ helpers do
     $db
   end
   def count()
-    $db.fetch("select count(*) from set").all[0][:count]
+    $db.fetch("select count(*) from set").first[:count]
   end
 
   def get_file_by_set_id(id, &blk)
@@ -24,6 +24,9 @@ helpers do
                   and file.id = set_file.file_id
                   and file.image = true
                   and set.id = ? order by set_file.pos", id) { |r| blk.call(r) }
+  end
+  def list_sets_by_page(gap, n, &blk)
+    db.fetch("select id, dir from set limit ? offset ?", gap, n * gap) { |r| blk.call(r) }
   end
 end
 
@@ -39,19 +42,23 @@ get '/page/:n' do |n|
   @n = Integer(n)
   @N = count()
   @gap = 100
+  @last = @N / @gap
+  @win = 21
+  @half = 10
 
-
-  @min = 0
-  @max = (@N / @gap)
-
-  if @n > 20 then
-    @min = @n - 20
-    @hasless = true
+  if @n < @win then
+    @min = 0
+    @max = @win
+  elsif @last - @n < @win then
+    @min = @last - @win
+    @max = @last
+  else
+    @min = @n - @half
+    @max = @n + @half
   end
-  if @max - @n > 20 then
-    @max = @n + 20
-    @hasmore = true
-  end
+
+  @hasless = true if @min > 0
+  @hasmore = true if @max < @last
 
   haml :page
 end
@@ -70,8 +77,13 @@ __END__
 
 @@ page
 %a{ :href => "/all" } All
+|
 
-="..." if @hasless
+-if @n > 0
+  %a{ :href => "/page/0" } <<
+- if @hasless
+  %a{ :href => "/page/#{@n - @win}" } <
+
 - for i in @min .. @n-1
   %a{ :href => "/page/#{i}" }=i
 
@@ -79,17 +91,13 @@ __END__
 
 - for i in @n+1 .. @max
   %a{ :href => "/page/#{i}" }=i
-="..." if @hasmore
 
+- if @hasmore
+  %a{ :href => "/page/#{@n + @win}" } >
+- if @n < @last
+  %a{ :href => "/page/#{@last}" } >>
 
-- db.fetch("select id, dir from set limit #{@gap} offset #{@n * @gap}") do |r|
-  %p
-    %a{ :href => "/set/#{r[:id]}" }
-      =File.basename r[:dir]
-
-@@ all
-%p=count()
-- db.fetch("select id, dir from set") do |r|
+- list_sets_by_page(@gap, @n) do |r|
   %p
     %a{ :href => "/set/#{r[:id]}" }
       =File.basename r[:dir]
@@ -99,3 +107,10 @@ __END__
 %br
 - get_file_by_set_id(@id) do |r|
     %img{ :src => "http://127.0.0.1:4567#{r[:path]}" }
+
+@@ all
+%p=count()
+- db.fetch("select id, dir from set") do |r|
+  %p
+    %a{ :href => "/set/#{r[:id]}" }
+      =File.basename r[:dir]
