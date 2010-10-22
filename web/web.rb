7c97a5286b04212b8f19a5654a495bf7c35d8bce
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2010-10-22 06:43:16 -0300

    random improvements, layout

--- web/web.rb
+++ web/web.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sequel'
 require 'sinatra'
+require 'sass'
 
 set :environment, :production
 
@@ -11,22 +12,20 @@ set :port, 1025
 $db = Sequel.connect('postgres://localhost')
 
 helpers do
-  def db
-    $db
-  end
   def count()
     $db.fetch("select count(*) from set").first[:count]
   end
 
   def get_file_by_set_id(id, &blk)
-    db.fetch("select file.path from set, file, set_file
-                where set.id = set_file.set_id
-                  and file.id = set_file.file_id
+    $db.fetch("select file.path from file, set_file
+                where file.id = set_file.file_id
                   and file.image = true
-                  and set.id = ? order by set_file.pos", id) { |r| blk.call(r) }
+                  and set_file.set_id = ?
+                order by set_file.pos", id) { |r| blk.call(r) }
   end
+
   def list_sets_by_page(gap, n, &blk)
-    db.fetch("select id, dir from set limit ? offset ?", gap, n * gap) { |r| blk.call(r) }
+    $db.fetch("select id, dir from set limit ? offset ?", gap, n * gap) { |r| blk.call(r) }
   end
 end
 
@@ -39,12 +38,14 @@ get '/all' do
 end
 
 get '/page/:n' do |n|
+  @gap = 100
+  @half = 10
+
   @n = Integer(n)
   @N = count()
-  @gap = 100
+
   @last = @N / @gap
-  @win = 21
-  @half = 10
+  @win = 2*@half + 1
 
   if @n < @win then
     @min = 0
@@ -68,39 +69,47 @@ get '/set/:id' do |id|
   haml :set
 end
 
+get '/style.css' do
+  sass :style
+end
+
 enable :inline_templates
 
 __END__
 
 @@ layout
-= yield
+%html
+  %head
+    %link{:href=>'/style.css', :rel => 'stylesheet', :type => "text/css"}
+  %body
+    = yield
 
 @@ page
-%a{ :href => "/all" } All
-|
+#menu
+  %a{ :href => "/all" } All
+  |
+  -if @n > 0
+    %a{ :href => "/page/0" } <<
+  - if @hasless
+    %a{ :href => "/page/#{@n - @win}" } <
 
--if @n > 0
-  %a{ :href => "/page/0" } <<
-- if @hasless
-  %a{ :href => "/page/#{@n - @win}" } <
+  - for i in @min .. @n-1
+    %a{ :href => "/page/#{i}" }=i
 
-- for i in @min .. @n-1
-  %a{ :href => "/page/#{i}" }=i
+  =@n
 
-=@n
+  - for i in @n+1 .. @max
+    %a{ :href => "/page/#{i}" }=i
 
-- for i in @n+1 .. @max
-  %a{ :href => "/page/#{i}" }=i
-
-- if @hasmore
-  %a{ :href => "/page/#{@n + @win}" } >
-- if @n < @last
-  %a{ :href => "/page/#{@last}" } >>
+  - if @hasmore
+    %a{ :href => "/page/#{@n + @win}" } >
+  - if @n < @last
+    %a{ :href => "/page/#{@last}" } >>
 
 - list_sets_by_page(@gap, @n) do |r|
   %p
     %a{ :href => "/set/#{r[:id]}" }
-      =File.basename r[:dir]
+      =r[:dir].sub %r{.*/([^/]+/[^/]+/[^/]+)}, '\1'
 
 @@ set
 %a{ :href => '/' } "Up"
@@ -110,7 +119,12 @@ __END__
 
 @@ all
 %p=count()
-- db.fetch("select id, dir from set") do |r|
+- $db.fetch("select id, dir from set") do |r|
   %p
     %a{ :href => "/set/#{r[:id]}" }
       =File.basename r[:dir]
+
+@@ style
+#menu
+  font-family: Andale Mono, monospace
+  font-size: 130%
