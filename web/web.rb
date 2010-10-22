547d0a6dfbb752a8632ae50a24f0face7047fa78
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2010-10-21 23:42:57 -0300

    Adding some rudimentary menu

--- web/web.rb
+++ web/web.rb
@@ -5,17 +5,50 @@ require 'sinatra'
 set :environment, :production
 
 set :run, true
+set :bind, '127.0.0.1'
 set :port, 1025
 
-db = Sequel.connect('postgres://localhost')
+$db = Sequel.connect('postgres://localhost')
+
+helpers do
+  def db
+    $db
+  end
+  def count()
+    $db.fetch("select count(*) from set")[''][:count]
+  end
+end
 
 get '/' do
-  @db = db
-  haml :index
+  redirect '/page/0'
+end
+
+get '/all' do
+  haml :all
+end
+
+get '/page/:n' do |n|
+  @n = Integer(n)
+  @N = count()
+  @gap = 100
+
+
+  @min = 0
+  @max = (@N / @gap)
+
+  if @n > 20 then
+    @min = @n - 20
+    @hasless = true
+  end
+  if @max - @n > 20 then
+    @max = @n + 20
+    @hasmore = true
+  end
+
+  haml :page
 end
 
 get '/set/:id' do |id|
-  @db = db
   @id = id
   haml :set
 end
@@ -27,14 +60,34 @@ __END__
 @@ layout
 = yield
 
-@@ index
-- @db.fetch("select id, dir from set") do |r|
+@@ page
+%a{ :href => "/all" } All
+
+="..." if @hasless
+- for i in @min .. @n-1
+  %a{ :href => "/page/#{i}" }=i
+
+=@n
+
+- for i in @n+1 .. @max
+  %a{ :href => "/page/#{i}" }=i
+="..." if @hasmore
+
+
+- db.fetch("select id, dir from set limit #{@gap} offset #{@n * @gap}") do |r|
   %p
-    %a{ :href => "set/#{r[:id]}" }
+    %a{ :href => "/set/#{r[:id]}" }
+      =File.basename r[:dir]
+
+@@ all
+%p=count()
+- db.fetch("select id, dir from set") do |r|
+  %p
+    %a{ :href => "/set/#{r[:id]}" }
       =File.basename r[:dir]
 
 @@ set
 %a{ :href => '/' } "Up"
 %br
-- @db.fetch("select path from file where image = true and id in (select file_id from set_file where set_id = #{@id} order by pos desc)") do |r|
+- db.fetch("select path from file where image = true and id in (select file_id from set_file where set_id = #{@id} order by pos desc)") do |r|
     %img{ :src => "http://127.0.0.1:4567#{r[:path]}" }
